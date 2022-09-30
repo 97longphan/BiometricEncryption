7de5f2bd52a1d23c9309264188e6daba74c51548
long.phan <long.phan@onemount.com>
2022-09-30 15:53:06 +0700

    update

--- BiometricEncryption/ViewController.swift
+++ BiometricEncryption/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
-
+import Security
 class ViewController: UIViewController {
-
+    private var keychainManager: KeyChainManager?
+    @IBOutlet weak var keyTF: UITextField!
+    @IBOutlet weak var valueTF: UITextField!
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createKeychainManager()
+        keyTF.resignFirstResponder()
     }
-
-
+    
+    func createKeychainManager() {
+        let keychain = KeyChain()
+        self.keychainManager = KeyChainManager(keychain)
+    }
+    
+    func addItemToKeychain(_ password: String) -> OSStatus? {
+        guard let data = password.data(using: .utf16) else { return nil }
+        return keychainManager?.save(key: keyTF.text ?? "", data: data)
+        
+    }
+    
+    @IBAction func addAction(_ sender: Any) {
+        let result = addItemToKeychain(valueTF.text ?? "")
+        itemLabel.text! += "saved value \(valueTF.text ?? "") has status: \(result ?? -0) \n"
+    }
+    
+    @IBAction func showItemAction(_ sender: Any) {
+        let data = keychainManager?.load(key: keyTF.text ?? "")
+        guard let data = data else {
+            itemLabel.text! += "value for \(keyTF.text ?? "") is nil \n"
+            return }
+        let value = String(data: data, encoding: .utf16)
+        itemLabel.text! += "value for \(keyTF.text ?? "") is \(value ?? "nil") \n"
+    }
+    
+    
+    
+    
 }
-
